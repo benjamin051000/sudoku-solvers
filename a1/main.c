29cc6e3cb534b982a46d4ae73997fb177536eb3a
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2022-08-30 02:23:49 -0400

    nearly figured out board 1d 2d thing. ugh

--- a1/main.c
+++ a1/main.c
@@ -13,6 +13,11 @@
 #define D(x)
 #endif
 
+// Standard sudoku board: 9x9.
+//const int NROWS = 9, NCOLS = NROWS;
+#define NROWS 9
+#define NCOLS 9
+
 /**
  * Use mmap to cleverly "read" the entire
  * contents of the file into memory.
@@ -24,42 +29,67 @@ char* read_file(const char* const filename) {
     const int fd = open(filename, O_RDONLY);
     const size_t len = lseek(fd, 0, SEEK_END);
     char* buf = mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
 
     if(buf == MAP_FAILED) {
-        printf("Error: memory-mapping failed!\n");
-        close(fd);
+    
         exit(EXIT_FAILURE);
     }
 
-    close(fd);
     return buf;
 }
 
 /**
  * Take the buffer and convert it to an array
  * of ints.
+ *
+ * NOTE: buf is non-const because
+ * strtok eats it.
+ *
+ * NOTE: Making this a 2-d array is actually
+ * really tricky. It requires a first row which
+ * is full of pointers to subsequent rows (since
+ * indexing is array[row][col]. It gets really messy,
+ * especially if you want to treat it like a 1-d array.
+ * I guess this is simply because the compiler doesn't
+ * realize it's a contiguous block and cannot decay
+ * the [][] into a single []. Oh well.
  */
 int* create_board(const char* const buf) {
-    char delim[] = ",\n"; 	
+    const char delim[] = ",\n"; 	
 
-    // Standard 9x9 sudoku board.
-    int *board = malloc(sizeof(int) * 9 * 9);
+    int *b = malloc(NROWS * NCOLS * sizeof(*b));
 
-    int i = 0, j = 0;
+    int i = 0;
 
-    char* token = strtok(buf, delim);
+    char* const s = strdup(buf);
+    char* token = strtok(s, (char*)delim);
     
-    while(token) {
+    while(token != NULL) {
         // A "better" version of atoi
         const int val = strtol(token, NULL, 10);
 
-        board[i++][j++] = val;
+        b[i++] = val;
 
-        strtok(buf, delim); // get next token
+        token = strtok(NULL, (char*)delim); // get next token
     }
 
-    return board;
+    return b;
 }
+
+D(
+void print_board(const int* const b) {
+    printf("printing board-------------\n");
+    for(int i = 0; i < NROWS; i++) {
+        for(int j = 0; j < NCOLS; j++) {
+            const int idx = i * NROWS + j;
+            printf("%d ", b[idx]); 
+        }
+        printf("\n");
+    }
+    printf("---------------------------\n");
+}
+)
 
 
 int main(int argc, char** argv) {
@@ -69,11 +99,18 @@ int main(int argc, char** argv) {
     }
 
 	// Get string input from stdin
-    char* buf = read_file(argv[1]);
+    const char* const buf = read_file(argv[1]);
+    int* b = create_board(buf);
+    // Copy 1-d board into 2-d thingy (TODO this may be no better than previous 2d board)
+    int board[NROWS][NCOLS];
+    memcpy(board, b, NROWS*NCOLS*sizeof(*b)); // don't trust!
+                                              // TODO see if board[][] works (I doubt it will)
 
-	printf("%s.\n", buf);
+    D(print_board((const int* const)board);)
 
-    free(buf);
+    // Cleanup
+    munmap((char*)buf, sizeof(buf));
+    free(b);
 	return 0;
 }
 
